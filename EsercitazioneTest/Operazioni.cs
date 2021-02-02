c3bf3e95dfc4afe363978f8af5563074c4fb2336
andry <andry@DESKTOP-SENM7HG>
2021-02-02 12:32:40 +0100

    Ho aggiunto delle verifiche per ogni test.

--- EsercitazioneTest/Operazioni.cs
+++ EsercitazioneTest/Operazioni.cs
@@ -16,6 +16,70 @@ namespace EsercitazioneTest
             Assert.AreEqual(valore_aspettato , resp);
         }
 
+        [TestMethod]
+        public void TestCalcoloMisterioso1()
+        {
+            int n = 6;
+            long valore_aspettato = 1440;
+            long resp = Calc.CalcoloMisterioso(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+
+        [TestMethod]
+        public void TestCalcoloMisterioso2()
+        {
+            int n = 3;
+            long valore_aspettato = 12;
+            long resp = Calc.CalcoloMisterioso(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+
+        [TestMethod]
+        public void TestCalcoloMisterioso3()
+        {
+            int n = 2;
+            int valore_aspettato = -6;
+            long resp = Calc.CalcoloMisterioso2(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+
+        [TestMethod]
+        public void TestCalcoloMisterioso4()
+        {
+            int n = 5;
+            int valore_aspettato = -360;
+            long resp = Calc.CalcoloMisterioso2(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+
+        [TestMethod]
+        public void TestCalcoloMisterioso5()
+        {
+            int n = 7;
+            int valore_aspettato = -15120;
+            long resp = Calc.CalcoloMisterioso2(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+
+        [TestMethod]
+        public void TestMisurazioneMisteriosa()
+        {
+            double[] valori = new double[] { 4, 6, 7, -9.1 };
+            double valore_aspettato = 3;
+            double min = Calc.MisurazioneMisteriosa(valori);
+            Assert.AreEqual(valore_aspettato, min);
+        }
+
+        [TestMethod]
+        public void TestMisurazioneMisteriosa1()
+        {
+            double[] valori = new double[] { 8, 10.25, -5.2, 9.6 };
+            double valore_aspettato = 2;
+            double min = Calc.MisurazioneMisteriosa(valori);
+            Assert.AreEqual(valore_aspettato, min);
+        }
+
+
     }
 }
 
--- Gestione/Calc.cs
+++ Gestione/Calc.cs
@@ -18,5 +18,33 @@ namespace Gestione
             return valore;
         }
 
+        public static long CalcoloMisterioso2(int n)
+        {
+
+            long valore = -3;
+            if (n == 0)
+                valore = 0;
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                    valore = valore * i;
+            }
+
+            return valore;
+        }
+
+        public static double MisurazioneMisteriosa(double[] valori)
+        {
+
+            double a = 0;
+            for (int i = 0; i < valori.Length; i++)
+            {
+                if (valori[i] < a)
+                    a = i;
+            }
+            return a;
+        }
+
+
     }
 }
